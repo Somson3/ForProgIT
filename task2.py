d6bf59030641a8bb33fa33bccd1720bf0acde605
unknown <s-camermen@ukr.net>
2022-12-10 20:27:00 +0200

    Home work 2/2 and 3

--- task2.py
+++ task2.py
@@ -1,123 +1,23 @@
-# Exercise 1.
-# print("Hello world")
-#
-# # Exercise 2._______________________________________________
-#
-# a = "I study programming"
-# print(a)
-# b = "Programming in Python is interesting"
-# print(b)
-# c = "The war is raging. Help."
-# print(c)
 
-# Exercise 4._____________________________________________
-
-# a = int(input(" Enter an integer "))
-# b = int(input(" Enter an integer "))
-# c = a + b
-# d = a * b
-# i = a - b
-# f = a / b
-# print(f"sum {c}, product {d}, difference {i}, quotient {f}")
-
-
-# Exercise 3._____________________________________________
-
-# a = int(input(" Enter he width of the rectangle  "))
-# b = int(input(" Enter the length of the rectangle  "))
-# s = a * b
-# print("Area of rectangle", s)
-
-
-# Exercise 5._____________________________________________
-
-# radius = int(input("Enter radius:"))
-# Pi = 3.14159
-# if radius > 0:
-#     print("Длина окружности: ", 2*Pi*radius)
-#     print("Площадь равна =  ", Pi*radius**2)
-# else:
-#     print("Введите положительное число")
-
-# Домашнє завдання 2
+# Домашнє завдання 2\другий рівень
 
 # Exercise 1._____________________________________________
-
-# a = "123"
-# print(int(a))
-
-# Exercise 2._____________________________________________
-
-# a = 123
-# print(float(a))
-#
-# Exercise 3._____________________________________________
-
-# a = 12.345
-# print(int(a))
-
-# Exercise 4._____________________________________________
-
-# bank_card = 2075_2580_3045_4560_8555
-# print(bank_card % 10000)
-
-# Exercise 5._____________________________________________
-
-# a = input("Введіть число ")
-# print(sum(map(int, a)))
-
-# Exercise 6._____________________________________________
-
-# a = int(input("Введіть число "))
-# b = int(input("Введіть число "))
-# c = int(input("Введіть число "))
-#
-# p = (a + b + c) / 2
-# plosha = (p * (p - a)*(p - b)*(p - c) ** 0.5)
-# print(plosha)
-
-# Exercise 6_1._____________________________________________
-
-# x = input("Введіть число  ")
-# print(len(x))
-
-# Exercise 6_2._____________________________________________
-
-# x = input("Введіть число  ")
-# print(x[::-1])
-
-
-# Домашнє завдання 3
-
-# Exercise 1._____________________________________________
-
-# x = int(input("Введіть відємне число   "))
-# if x < 0 and x != 0:
-#     print(x)
-# else:
-#     print(" Ви не ввели відємне число")
-
-
+# x = int(input("Введіть відємне число "))
+# res = x < 0 or "Число не меньше за нуль"
+# print(res)
 # Exercise 2._____________________________________________
 
 # a = int(input("Введіть число "))
-# if a < 20 and a != 0:
-#     print(f"Число {a} меньше  20 ")
-# elif a == 0:
-#     print(f"Число {a} меньше  20 ")
-# elif a == 20:
-#     print(f"Число {a} = 20 ")
-# else:
-#     print(f"Число {a} більше 20")
+# x = a <= 20 or "Число більше 20"
+# print(x)
+
+
 
 # Exercise 3._____________________________________________
 
 # a = int(input("Введіть число  "))
-# if a != 0:
-#     print(f"Введе число {a} не дорівнює 0  ")
-# elif a == 0:
-#     print(f"Введе число {a} дорівнює 0  ")
-
+# m = a == 0,"Введене число дорінює нулю!" or " Введене число не дорінює нулю!"
+# print(m)
 # Exercise 4._____________________________________________
 
 # a = int(input("Введіть число  "))
@@ -131,10 +31,44 @@
 # a = int(input("Введіть число  "))
 # b = int(input("Введіть число  "))
 # c = int(input("Введіть число  "))
+# s = max(a, b, c)
+# print("Max is ", s)
+
+
+
+# Домашка 3
+
+# Exercise 1._____________________________________________
+
+# flat_num = int(input("Введіть номер квартири  "))
+# if 0 <= flat_num <= 144:
+#     if flat_num % 36 == 0:
+#         floor = 9
+#         entrance = flat_num // 36
+#     if flat_num % 36 != 0:
+#         entrance = (flat_num // 36) + 1
+#         floor = (flat_num % 36) // 4 + 1
+#     print(f" Номер квартири {flat_num} знаходиться в під`їзді № {entrance} на {floor} поверсі.")
+# else:
+#     print("Квартири з таким номером немає ")
+
+# Exercise 2._____________________________________________
+
+
+# year = int(input("Ввеждіть кількість днів у році  "))
 #
-# if a > b > c:
-#     print(a)
-# elif b > a > c:
-#     print(b)
-# elif c > b > a:
-#     print(c)
+# if year % 4 and not year % 100 or year % 400:
+#     print("Рік високосний ")
+# else:
+#     print("Рік не високосний ")
+
+# Exercise 3._____________________________________________
+
+# a = int(input("Введіть значення "))
+# b = int(input("Введіть значення "))
+# c = int(input("Введіть значення "))
+#
+# if a + b > c or b + c > a or c + a > b:
+#     print("Трикутник існує ")
+# else:
+#     print("Трикуиник не існує ")
